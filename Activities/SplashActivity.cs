0a4a9835978c25dff02f9f382830872ac9bafcc5
Yonka <yonka2003@gmail.com>
2024-01-22 03:32:13 +0200

    fixed

--- Activities/SplashActivity.cs
+++ Activities/SplashActivity.cs
@@ -21,7 +21,7 @@ namespace OrdersExtractor.Activities
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
-
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
 
